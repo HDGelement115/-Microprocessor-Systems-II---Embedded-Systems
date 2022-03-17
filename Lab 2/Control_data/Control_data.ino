f0d67ede1a6c09ab6836418fd3be6c6a5adacabb
Michael Bautista (Tower) <Michael_Bautista@student.uml.eduMic>
2022-03-17 07:27:25 -0400

    Sketch and Snake
    -added code to an Arduino file to get data from sensors and read data from a python file using serial
    
    -added code to snake.py file to be able to actually use the data from the Arduino Sensors to play the snake game.

---
+++ Lab 2/Control_data/Control_data.ino
@@ -0,0 +1,82 @@
+#include<Wire.h>
+const int MPU=0x68; 
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int VRx = A0;
+int VRy = A1;
+int SW = 2;
+
+int xPosition = 0;
+int yPosition = 0;
+int SW_state = 0;
+int mapX = 0;
+int mapY = 0;
+
+int eaton;
+int buzzer = 3;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); 
+  Wire.write(0);    
+  Wire.endTransmission(true);
+  Serial.begin(250000);
+  pinMode(VRx, INPUT);
+  pinMode(VRy, INPUT);
+  pinMode(SW, INPUT_PULLUP); 
+  pinMode(buzzer, OUTPUT);
+}
+void loop(){
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14,true);    //each measurement has 2 registers
+  AcX=Wire.read()<<8|Wire.read();    
+  AcY=Wire.read()<<8|Wire.read();  
+  AcZ=Wire.read()<<8|Wire.read();
+  Tmp = Wire.read()<<8 | Wire.read();  
+  GyX=Wire.read()<<8|Wire.read();  
+  GyY=Wire.read()<<8|Wire.read();  
+  GyZ=Wire.read()<<8|Wire.read();  
+
+  xPosition = analogRead(VRx);
+  yPosition = analogRead(VRy);
+  SW_state = digitalRead(SW);
+  mapX = map(xPosition, 0, 1023, -512, 512);
+  mapY = map(yPosition, 0, 1023, -512, 512);
+
+  Serial.print("Joystick: ");
+  Serial.print("X: ");
+  Serial.print(mapX);
+  Serial.print(" | Y: ");
+  Serial.print(mapY);
+  Serial.print(" | Button: ");
+  Serial.println(SW_state);
+  
+  Serial.print("Accelerometer: ");
+  Serial.print("X = "); Serial.print(AcX);
+  Serial.print(" | Y = "); Serial.print(AcY);
+  Serial.print(" | Z = "); Serial.println(AcZ); 
+
+  //Serial.print("Tempature: ");
+  //Serial.println(Tmp/340.00+36.53);
+  
+  Serial.print("Gyroscope: ");
+  Serial.print("X = "); Serial.print(GyX);
+  Serial.print(" | Y = "); Serial.print(GyY);
+  Serial.print(" | Z = "); Serial.println(GyZ);
+  
+  if (Serial.available() > 0) {
+    // read the oldest byte in the serial buffer:
+    eaton = Serial.read();
+    // if it's a capital H (ASCII 72), turn on the Buzzer;
+    if (eaton == 'H') {
+      digitalWrite(buzzer, HIGH);
+    }
+    // if it's an L (ASCII 76) turn off the Buzzer:
+    if (eaton == 'L') {
+      digitalWrite(buzzer, LOW);
+    }
+  }
+  delay(500);
+}
