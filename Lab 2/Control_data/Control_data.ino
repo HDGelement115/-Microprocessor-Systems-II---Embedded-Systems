5cf741182083341aa1209d130e7eca3b0127b3da
TenLhu59 <98798911+TenLhu59@users.noreply.github.com>
2022-03-23 14:41:38 -0400

    Update Control_data.ino

--- Lab 2/Control_data/Control_data.ino
+++ Lab 2/Control_data/Control_data.ino
@@ -1,9 +1,11 @@
+
 #include<Wire.h>
 const int MPU=0x68; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 int VRx = A0;
 int VRy = A1;
-int SW = 2;
+//int SW = 2;
+
 
 int xPosition = 0;
 int yPosition = 0;
@@ -13,6 +15,7 @@ int mapY = 0;
 
 int eaton;
 int buzzer = 3;
+//char tmp_str[7];
 
 void setup(){
   Wire.begin();
@@ -23,7 +26,7 @@ void setup(){
   Serial.begin(250000);
   pinMode(VRx, INPUT);
   pinMode(VRy, INPUT);
-  pinMode(SW, INPUT_PULLUP); 
+  //pinMode(SW, INPUT_PULLUP); 
   pinMode(buzzer, OUTPUT);
 }
 void loop(){
@@ -34,30 +37,40 @@ void loop(){
   AcX=Wire.read()<<8|Wire.read();    
   AcY=Wire.read()<<8|Wire.read();  
   AcZ=Wire.read()<<8|Wire.read();
-  Tmp = Wire.read()<<8 | Wire.read();  
-  GyX=Wire.read()<<8|Wire.read();  
-  GyY=Wire.read()<<8|Wire.read();  
-  GyZ=Wire.read()<<8|Wire.read();  
+  //Tmp = Wire.read()<<8 | Wire.read();  
+  //GyX=Wire.read()<<8|Wire.read();  
+  //GyY=Wire.read()<<8|Wire.read();  
+  //GyZ=Wire.read()<<8|Wire.read();  
 
   xPosition = analogRead(VRx);
   yPosition = analogRead(VRy);
-  SW_state = digitalRead(SW);
-  mapX = map(xPosition, 0, 1023, -512, 512);
-  mapY = map(yPosition, 0, 1023, -512, 512);
+  //SW_state = digitalRead(SW);
 
-  Serial.print("Joystick: ");
-  Serial.print("X: ");
-  Serial.print(mapX);
-  Serial.print(" | Y: ");
-  Serial.print(mapY);
-  Serial.print(" | Button: ");
-  Serial.println(SW_state);
-  
-  Serial.print("Accelerometer: ");
-  Serial.print("X = "); Serial.print(AcX);
-  Serial.print(" | Y = "); Serial.print(AcY);
-  Serial.print(" | Z = "); Serial.println(AcZ); 
+  // Maps joystick input from -5 to 5 with 0 as the origin (joystick not used)
+  mapX = map(xPosition + 18, 0, 1023, -5, 5);     
+  mapY = map(yPosition + 18, 0, 1023, -5, 5);
 
+  if (mapX || mapY ){   // Stops Joystick input to Serial if both mapX and mapY are 0 (Joystick not used)
+    Serial.print("Joystick: ");
+    Serial.print("X: ");
+    Serial.print(mapX);
+    Serial.print(" | Y: ");
+    Serial.print(mapY);
+    Serial.print("\n");
+    //Serial.print(" | Button: ");
+    //Serial.println(SW_state);
+  }
+
+  // Accelerometer data used for tilt detection. Ranges are defined based on MPU6050 raw data when used
+  if (AcX < 2000 && AcY > 1000)
+    Serial.println("MPU: 4"); //Down
+  else if (AcX < 2000 && AcY < -1000) 
+    Serial .println("MPU: 1"); //Up
+  else if (AcX < -1000 && AcY > -1000) 
+    Serial .println("MPU: 2"); //right
+  else if (AcX > 2000 && AcY > -1000) 
+    Serial .println("MPU: 3"); //left
+/*
   //Serial.print("Tempature: ");
   //Serial.println(Tmp/340.00+36.53);
   
@@ -65,7 +78,9 @@ void loop(){
   Serial.print("X = "); Serial.print(GyX);
   Serial.print(" | Y = "); Serial.print(GyY);
   Serial.print(" | Z = "); Serial.println(GyZ);
-
+  
+  */
+  
   //using a single flag to turn the buzzer on and off in the same go
   //~Ryans idea
   if (Serial.available() > 0) {
@@ -77,5 +92,5 @@ void loop(){
     }
 
   }
-  delay(500);
+  delay(200);
 }
